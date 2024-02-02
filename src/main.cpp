24286cdabadea4d5229bd41528424e44f512bbe6
Qiwei Mao <57202568+NomiBobby@users.noreply.github.com>
2024-02-01 23:56:53 -0500

    move statements to SPIFFS

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <SPIFFS.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <HTTPClient.h>
 
 const int LOG_INTERVAL = 60000;
 // WiFi
@@ -15,8 +16,9 @@ IPAddress staticIP(192, 168, 1, 100);  // Change this to your desired static IP
 IPAddress gateway(192, 168, 0, 1);    // Change this to your router's IP
 IPAddress subnet(255, 255, 255, 0);   // Change this to match your network's subnet mask
 
+
 const char *ntpServer = "pool.ntp.org";
-// const char *ntpServer = "time.google.com";
+const char *AWSEC2 = "lelelumon.shop";
 const long gmtOffset_sec = -5 * 60 * 60;  // GMT offset in seconds (Eastern Time Zone)
 const int daylightOffset_sec = 3600;       // Daylight saving time offset in seconds
 const char *filename = "/flash_size_log.txt";
@@ -28,59 +30,31 @@ void connectToWiFi();
 void WiFiEvent(WiFiEvent_t event);
 void reconnectToWiFi();
 void serveIndexPage(AsyncWebServerRequest *request);
+void serveCompleteFile(AsyncWebServerRequest *request);
+void stopHandshake(AsyncWebServerRequest *request);
+void sendIPToEC2();
+void setupSPIFFS();
+String getPublicIP();
 
 // Create an instance of the server
 AsyncWebServer server(80);
 
 void setup() {
   Serial.begin(115200);
+  connectToWiFi();// Set up WiFi connection
+  WiFi.onEvent(WiFiEvent);// Register the WiFi event handler
+  setupSPIFFS();// Setup SPIFFS
 
-  // Set up WiFi connection
-  connectToWiFi();
+  server.on("/all", HTTP_GET, serveCompleteFile);// Serve the text file
+  server.on("/", HTTP_GET, serveIndexPage);// Serve the index.html file
+  server.on("/ipconfirm", HTTP_POST, stopHandshake);// Serve the index.html file
+  server.begin();  // Start server
+  Serial.printf("Server Started @ IP: %s\n", WiFi.localIP().toString().c_str());
 
-  // Register the WiFi event handler
-  WiFi.onEvent(WiFiEvent);
-  
+  setUpTime();// Sync With NTP Time Server
 
-  // Mount SPIFFS filesystem
-  Serial.println("Mounting SPIFFS filesystem");
-  if (!SPIFFS.begin(true)) {
-    Serial.println("SPIFFS allocation failed");
-    return;
-  }
-
-  // Check if the file already exists
-  Serial.println("Checking if file exists");
-  if (!SPIFFS.exists(filename)) {
-    Serial.println("Doesn't exist");
-    // If the file doesn't exist, create it and write the header
-    File file = SPIFFS.open(filename, "w");
-    if (!file) {
-      Serial.println("Failed to open file for writing");
-      return;
-    }
-    file.println("Time, fSize, SPIFFS Used, SPIFFS Total");
-    Serial.println("Time, fSize, SPIFFS Used, SPIFFS Total");
-    file.close();
-  }
-
-  // Serve the text file
-  Serial.println("Setting Server Callback function:");
-  server.on("/all", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, filename, "text/plain");
-  });
-
-  // Serve the index.html file
-  server.on("/", HTTP_GET, serveIndexPage);
-
-
-  // Start server
-  Serial.println("Starting server...");
-  server.begin();
-  Serial.println("***Serve On***");
-
-  // Sync With NTP Time Server
-  setUpTime();
+  /* TO DO*/
+  // xTaskCreate((TaskFunction_t)sendIPToEC2, "Task1", 4096, NULL, 1, NULL);
 }
 
 void loop() {
@@ -137,18 +111,18 @@ void setUpTime(){
 }
 
 void connectToWiFi() {
-  Serial.println("Connecting to WiFi");
+  // Serial.println("Connecting to WiFi");
   // WiFi.config(staticIP, gateway, subnet);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Connecting to WiFi...");
+    // Serial.println("Connecting to WiFi...");
   }
 
-  Serial.println("Connected to WiFi");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
+  // Serial.println("Connected to WiFi");
+  // Serial.print("IP Address: ");
+  // Serial.println(WiFi.localIP());
 }
 
 void WiFiEvent(WiFiEvent_t event) {
@@ -173,7 +147,30 @@ void reconnectToWiFi() {
   connectToWiFi();
 }
 
+String getPublicIP() {
+  HTTPClient http;
+
+  // Make a GET request to api.ipify.org to get the public IP
+  http.begin("https://api.ipify.org");
+
+  // Send the request
+  int httpCode = http.GET();
+
+  // Check for a successful response
+  if (httpCode == HTTP_CODE_OK) {
+    String publicIP = http.getString();
+    return publicIP;
+  } else {
+    Serial.printf("HTTP request failed with error code %d\n", httpCode);
+    return "Error";
+  }
+
+  // End the request
+  http.end();
+}
+
 void serveIndexPage(AsyncWebServerRequest *request) {
+
   // Open the file in read mode
   File file = SPIFFS.open("/index.html", "r");
 
@@ -199,4 +196,68 @@ void serveIndexPage(AsyncWebServerRequest *request) {
 
   // Close the file
   file.close();
+}
+
+void serveCompleteFile(AsyncWebServerRequest *request){
+  Serial.println("Client requested complete file.");
+  request->send(SPIFFS, filename, "text/plain");
+}
+
+void sendIPToEC2() {
+  while(1){
+    WiFiClient client;
+    HTTPClient http;
+    
+
+    // Prepare the POST data
+    String postData = "ip=" + getPublicIP();
+    Serial.println("postData = " + postData);
+
+    http.begin(client,"http://lelelumon.shop/esp32handshake");
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    // Send the POST request
+    int httpResponseCode = http.POST(postData);
+
+    // Check the response
+    if (httpResponseCode > 0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    } else {
+      Serial.println("HTTP Request failed");
+    }
+
+    // Free resources
+    http.end();
+
+    delay(5000);
+  }
+}
+
+void stopHandshake(AsyncWebServerRequest *request){
+
+}
+
+void setupSPIFFS(){
+  // Mount SPIFFS filesystem
+  Serial.println("Mounting SPIFFS filesystem");
+  if (!SPIFFS.begin(true)) {
+    Serial.println("SPIFFS allocation failed");
+    return;
+  }
+
+  // Check if the file already exists
+  Serial.println("Checking if file exists");
+  if (!SPIFFS.exists(filename)) {
+    Serial.println("Doesn't exist");
+    // If the file doesn't exist, create it and write the header
+    File file = SPIFFS.open(filename, "w");
+    if (!file) {
+      Serial.println("Failed to open file for writing");
+      return;
+    }
+    file.println("Time, fSize, SPIFFS Used, SPIFFS Total");
+    Serial.println("Time, fSize, SPIFFS Used, SPIFFS Total");
+    file.close();
+  }
 }
