e871bf70b238d85053b3b8f6041c1411fd617634
Qiwei Mao <57202568+NomiBobby@users.noreply.github.com>
2024-02-18 13:04:07 -0500

    split callbacks into sections

--- src/api_interface.h
+++ src/api_interface.h
@@ -9,13 +9,17 @@ extern AsyncWebServer server;
 
 void startServer();
 
+// Frontend Handler
 void serveIndexPage(AsyncWebServerRequest *request);
 void serveJS(AsyncWebServerRequest *request);
 void serveCSS(AsyncWebServerRequest *request);
 void servefavicon(AsyncWebServerRequest *request);
 
+// Logger Control Handler
 void serveRebootLogger(AsyncWebServerRequest *request);
 void pauseLoggingHandler(AsyncWebServerRequest *request);
 void resumeLoggingHandler(AsyncWebServerRequest *request);
+
+// Data Request Handler
 
 #endif
