1e7798c7aadc7a915e1d6891d27e207c759c553e
Qiwei Mao <57202568+NomiBobby@users.noreply.github.com>
2024-02-23 00:23:02 -0500

    change interval

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include "datalogging.h"
 #include "utils.h"
 #include "api_interface.h"
+#include "VM_501.h" // delete after testing
+
 
 // Include the required libraries
 #include <HardwareSerial.h>
@@ -13,18 +15,22 @@
 HardwareSerial VM(1); // UART port 1 on ESP32
 
 /* User Input*/
-const char *AWSEC2 = "lelelumon.shop";
 const char *filename = "/log.txt";
 const long gmtOffset_sec = -5 * 60 * 60;  // GMT offset in seconds (Eastern Time Zone)
 const int daylightOffset_sec = 3600;  
-const int LOG_INTERVAL = 1000;
+const int LOG_INTERVAL = 3000;
 
 /* Do not modify below */
-bool loggingPaused = true;
+bool loggingPaused = false;
 SemaphoreHandle_t logMutex;
+TaskHandle_t parsingTask; // Task handle for the parsing task
+TaskHandle_t vmListeningTask; // Task handle for the VM listening task
 
 void setup() {
   Serial.begin(115200);
+  // 0x01 0x03 0x00 0x00 0x00 0x0A 0xC5 0xCD
+  xTaskCreatePinnedToCore(sendCommandVM501, "ParsingTask", 4096, NULL, 1, &parsingTask, 1);
+  // xTaskCreatePinnedToCore(VM501ListenTaskFunc, "ParsingTask", 4096, NULL, 1, &vmListeningTask, 1);
   VM.begin(9600, SERIAL_8N1, 16, 17); // Initialize UART port 1 with GPIO16 as RX and GPIO17 as TX
 
   Serial.println("-------------------------------------\nBooting...");
@@ -38,7 +44,7 @@ void setup() {
 
   startServer();// start Async server with api-interfaces
 
-  // setUpTime();// Sync With NTP Time Server
+  setUpTime();// Sync With NTP Time Server
   Serial.println("-------------------------------------");
   Serial.println("Data Acquisition Started...");
 
@@ -56,49 +62,16 @@ void loop() {
   ElegantOTA.loop();
 
   // Read data from UART port
-  if (VM.available()) {
-    String receivedChar = VM.readString();
-    Serial.print("VM501 Response:\n");
-    Serial.println(receivedChar);
-  }
+  // if (VM.available()) {
+  //   String receivedChar = VM.readString();
+  //   Serial.print("VM501 Response:\n");
+  //   Serial.println(receivedChar);
+  // }
 
 
-  // Read Data from PC Keyboard
-  if (Serial.available()) {
-    // Read input from Serial Monitor
-    String input = Serial.readStringUntil('\n');
-
-    // Hexadecimal data to store
-    uint8_t hexData[] = {0x01, 0x03, 0x00, 0x00, 0x00, 0x30, 0xC5, 0xCD};
-    VM.write(hexData, sizeof(hexData));
-
-    Serial.println("Sent to VM");
-    /* VM Response below*/
-    // Check if data is available to read
-    uint32_t count = 0;
-    while (VM.available() > 0) {
-      // Read the big endian bytes
-      uint8_t byte = VM.read(); // Most significant byte
-      
-      // Print out the received value in hex
-      if(count%2 == 0){
-        Serial.printf("0x");
-        if(count < 0x10){
-          Serial.printf("0");
-        }
-        Serial.printf("%X",count);
-      }
-
-      Serial.printf("0x");
-      if (byte < 0x10) Serial.print("0"); // Print leading zero if necessary
-      Serial.printf("%X", byte);
-
-      if(count%2 == 1){
-        Serial.println();
-      }
-
-    }
-
-  }
+  // // Read Data from PC Keyboard
+  // if (Serial.available()) {
+  //   sendMODBUSCommand(0x01, 0x03, 0x00, 0x00, 0x00, 0x0A, 0xC5, 0xCD);
+  // }
 
 }
