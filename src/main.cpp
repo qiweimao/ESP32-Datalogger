d308879e3e927679af3797f0d03415f3034d2b1b
Qiwei Mao <57202568+NomiBobby@users.noreply.github.com>
2024-02-25 00:08:51 -0500

    reorder boot process

--- src/main.cpp
+++ src/main.cpp
@@ -27,34 +27,32 @@ TaskHandle_t parsingTask; // Task handle for the parsing task
 TaskHandle_t vmListeningTask; // Task handle for the VM listening task
 
 void setup() {
-  Serial.begin(115200);
-  xTaskCreatePinnedToCore(sendCommandVM501, "ParsingTask", 4096, NULL, 1, &parsingTask, 1);
-  VM.begin(9600, SERIAL_8N1, 16, 17); // Initialize UART port 1 with GPIO16 as RX and GPIO17 as TX
-
-  initializeOLED();
-
+  /* Essentials for Remote Access */
   Serial.println("-------------------------------------\nBooting...");
+  Serial.begin(115200);
+  setupSPIFFS();// Setup SPIFFS -- Flash File System
+  SD_initialize();//SD card file system initialization
   logMutex = xSemaphoreCreateMutex();  // Initialize the mutex
 
   connectToWiFi();// Set up WiFi connection
   WiFi.onEvent(WiFiEvent);// Register the WiFi event handler
-
-  setupSPIFFS();// Setup SPIFFS -- Flash File System
-  SD_initialize();//SD card file system initialization
-
   startServer();// start Async server with api-interfaces
 
+  /* Logging Capabilities */
+  xTaskCreatePinnedToCore(sendCommandVM501, "ParsingTask", 4096, NULL, 1, &parsingTask, 1);
+  VM.begin(9600, SERIAL_8N1, 16, 17); // Initialize UART port 1 with GPIO16 as RX and GPIO17 as TX
   setUpTime();// Sync With NTP Time Server
+  initializeOLED();
+
   Serial.println("-------------------------------------");
   Serial.println("Data Acquisition Started...");
-
 }
 
 void loop() {
 
   if(!loggingPaused){
     if (xSemaphoreTake(logMutex, portMAX_DELAY)) {
-      LogErrorCode result = logFlashSize();
+      LogErrorCode result = logData();
       xSemaphoreGive(logMutex);
     }
   }
