92b1d4150ce4b2c0418357cc22a73ff7b01cbef1
Qiwei Mao <57202568+NomiBobby@users.noreply.github.com>
2024-05-29 23:00:01 -0400

    add gateway folder structure

--- src/lora_gateway.cpp
+++ src/lora_gateway.cpp
@@ -1,6 +1,7 @@
 #include "lora_gateway.h"
 #include "lora_peer.h"
 #include "configuration.h"
+#include "utils.h"
 
 struct_message incomingReadings;
 struct_pairing pairingDataGateway;
@@ -11,6 +12,7 @@ void OnDataRecvGateway(const uint8_t *incomingData, int len) {
   String payload;
   uint8_t type = incomingData[0];       // first message byte is the type of message 
   switch (type) {
+
     case DATA :                         // the message is data type
       memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
       root["mac"] = incomingReadings.mac;      // create a JSON document with received data and send it by event to the web page
@@ -24,11 +26,13 @@ void OnDataRecvGateway(const uint8_t *incomingData, int len) {
       break;
     
     case PAIRING:                            // the message is a pairing request 
+
       memcpy(&pairingDataGateway, incomingData, sizeof(pairingDataGateway));
 
       Serial.print("\nPairing request from: ");
       printMacAddress(pairingDataGateway.mac);
       Serial.println();
+      Serial.println(pairingDataGateway.deviceName);
 
       /* OLED for Dev */
       oled_print(pairingDataGateway.msgType);
@@ -36,15 +40,37 @@ void OnDataRecvGateway(const uint8_t *incomingData, int len) {
       oled_print(pairingDataGateway.mac[0]);
 
       if (pairingDataGateway.msgType == PAIRING) { 
+
         if(pairingDataGateway.pairingKey == PAIRING_KEY){
+
           Serial.println("Correct PAIRING_KEY");
+
           oled_print("send response");
-          Serial.println("Sending packet...");
-          LoRa.beginPacket();
-          LoRa.write((uint8_t *) &pairingDataGateway, sizeof(pairingDataGateway));
-          LoRa.endPacket();
-          addPeerGateway(pairingDataGateway.mac, pairingDataGateway.deviceName);
-          LoRa.receive();
+          sendLoraMessage((uint8_t *) &pairingDataGateway, sizeof(pairingDataGateway));
+          Serial.println("Sent pairing response...");
+
+          // If first time pairing, create a dir for this node
+          if(addPeerGateway(pairingDataGateway.mac, pairingDataGateway.deviceName)){
+            
+            Serial.println("First time pairing, create a dir for this node");
+
+            char deviceFolder[11]; // 10 for the name + 1 for the null terminator
+            strncpy(deviceFolder, pairingDataGateway.deviceName, 10);
+            deviceFolder[10] = '\0'; // Ensure null-termination
+
+            char folderPath[16]; // 1 for '/' + 4 for 'data' + 1 for '/' + 10 for the name + 1 for the null terminator
+            snprintf(folderPath, sizeof(folderPath), "/node/%s", deviceFolder);
+            Serial.println(folderPath);
+
+            if (SD.mkdir(folderPath)) {
+              Serial.println("Directory created successfully.");
+            } else {
+              Serial.println("Failed to create directory.");
+            }
+
+            Serial.println("End of dir creation.");
+          }
+
         }
         else{
           Serial.println("Wrong PAIRING_KEY");
@@ -67,6 +93,18 @@ void lora_gateway_init() {
   LoRa_rxMode();
 
   loadPeersFromSD();
+
+  // Ensure the "data" directory exists
+  if (!SD.exists("/node")) {
+    Serial.print("Creating 'node' directory - ");
+    if (SD.mkdir("/node")) {
+      Serial.println("OK");
+    } else {
+      Serial.println("Failed to create 'node' directory");
+    }
+  } else {
+    Serial.println("'node' directory already exists");
+  }
 
   xTaskCreate(taskReceive, "Data Handler", 10000, (void*)OnDataRecvGateway, 1, NULL);
 
