40db71e573a211340166af31d7d1e525508821c7
Qiwei Mao <57202568+NomiBobby@users.noreply.github.com>
2024-06-21 15:02:35 -0400

    add checks to avoid file transmission conflict

--- src/lora_file_transfer.cpp
+++ src/lora_file_transfer.cpp
@@ -245,13 +245,30 @@ String current_file_path;
 size_t total_bytes_received;
 size_t total_bytes_written;
 size_t bytes_written;
-file_meta_message file_meta_gateway;
 
 // ***********************
 // * Handle File Meta
 // ***********************
 void handle_file_meta(const uint8_t *incomingData){
+  
+
+  /* Need to verify if node has the permission to end file transmission */
+  // Add if encounters collision, current one to many design assumes node
+  // would not reach this point before getting rejected
+  /* Need to verify if node has the permission to end file transmission */
+
+  file_meta_message file_meta_gateway;
   memcpy(&file_meta_gateway, incomingData, sizeof(file_meta_gateway));
+
+  // If file path is not empty, means previous transmission has not ended yet
+  if(current_file_path != ""){
+    Serial.println("Reject file Meta, already in transmission.");
+    reject_message reject_message_gateway;
+    reject_message_gateway.msgType = REJ;
+    memcpy(&reject_message_gateway.mac, file_meta_gateway.mac, sizeof(file_meta_gateway.mac));
+    sendLoraMessage((uint8_t *) &reject_message_gateway, sizeof(reject_message_gateway));
+    return;
+  }
 
   // Get File Name
   char buffer[MAX_FILENAME_LEN + 1]; // 10 for the name + 1 for the null terminator
@@ -301,17 +318,24 @@ void handle_file_meta(const uint8_t *incomingData){
 // * Handle File Body
 // ***********************
 void handle_file_body(const uint8_t *incomingData){
+
+  /* Need to verify if node has the permission to end file transmission */
+  // Add if encounters collision, current one to many design assumes node
+  // would not reach this point before getting rejected
+  /* Need to verify if node has the permission to end file transmission */
+
   file_body_message file_body_gateway;
   memcpy(&file_body_gateway, incomingData, sizeof(file_body_gateway));
   Serial.println("Received FILE_BODY.");
 
-  // If file path is not set
+  // If file path is not set, send REJ and abort transmission
   if(current_file_path == ""){
     Serial.println("Reject file body, no path set.");
     reject_message reject_message_gateway;
     reject_message_gateway.msgType = REJ;
     memcpy(&reject_message_gateway.mac, file_body_gateway.mac, sizeof(file_body_gateway.mac));
     sendLoraMessage((uint8_t *) &reject_message_gateway, sizeof(reject_message_gateway));
+    return;
   }
 
   Serial.println(current_file_path);
@@ -333,7 +357,7 @@ void handle_file_body(const uint8_t *incomingData){
   
   ack_message ackMessage_gateway;
   ackMessage_gateway.msgType = ACK;
-  memcpy(&ackMessage_gateway.mac, file_body_gateway.mac, sizeof(file_meta_gateway.mac));
+  memcpy(&ackMessage_gateway.mac, file_body_gateway.mac, sizeof(file_body_gateway.mac));
   sendLoraMessage((uint8_t *) &ackMessage_gateway, sizeof(ackMessage_gateway));
 
   Serial.println("Data written to file successfully");
@@ -350,6 +374,8 @@ int handle_file_end(const uint8_t *incomingData){
   /* Need to verify if node has the permission to end file transmission */
   file_end_message file_end_gateway;
   memcpy(&file_end_gateway, incomingData, sizeof(file_end_gateway));
+
+
   Serial.print("Received FILE_END from:");
   printMacAddress(file_end_gateway.mac); Serial.println();
   Serial.print("Total bytes received: "); Serial.println(total_bytes_received);Serial.println();
